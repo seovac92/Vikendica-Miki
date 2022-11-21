5f444eceef895a40dcec423c89ba6221fe8ab519
seovacmarko5 <seovacmarko5@gmail.com>
2022-11-21 19:31:45 +0100

    completed about page

--- vikendica-miki/pages/about.js
+++ vikendica-miki/pages/about.js
@@ -1,8 +1,40 @@
+import Image from 'next/image'
+
 const About = () => {//stranica o nama,mozda neki side sa zanimljivostima u okolini!!!
+    //react cloud
+    //https://github.com/GoranKukic/portfolio/blob/main/src/components/Skills/icons.js
+    //obavezno uraditi!!!
     return ( 
         <div className="about-wrapper">
-            <main></main>
-            <aside></aside>
+            <main>
+                <div className="about-hero hero-wrapper">
+                    <div className="hero-text-wrapper">
+                        <h1>ВИКЕНДИЦА<br/>МИКИ</h1>
+                        <p>Уживајте у лепотама<br/>Таре</p>
+                    </div>
+                </div>
+            </main>
+            <aside>
+                <article className="first-container">
+                    <h2>О нама</h2>
+                    <p>Већ више од 10 година се успешно бавимо туризмом и представљамо на прави начин наш град и природне лепоте овог краја, које остављају без даха и очаравају многе, како домаће, тако и стране туристе. После дугогодишњег рада у овом послу и бројних гостију који су се у већини случајева враћали сваке године и тиме нам одавали захвалност за наш предан рад, стекли смо огромно искуство са којим је и квалитет наших услуга константно растао, тако да су увек и изнова били изненађени на исти начин, као када су први пут дошли. У свему томе нам је умногоме помогла и сама природа која је непресушан извор прелепих пејзажа и могућности да се они кроз различите активности доживе на прави начин и заувек остану у сећању.</p>
+                </article>
+                <article className="second-container">
+                <div className="image-container">
+                    <Image className='profile-image' src="/profile.jpg" alt="mountain house" width={500} height={300}></Image>
+                </div>
+                <div className="family-business-container">
+                    <h2>Викендица Мики</h2>
+                    <ul>
+                        <li>Приземље (дневни боравак, кухиња, трпезарија, спаваћа соба, купатило)</li>
+                        <li>Спрат (три спаваће собе и купатило)</li>
+                        <li>Испред је веранда са озиданим роштиљем</li>
+                        <li>Капацитет до 13 особа</li>
+                        <li>Потпуно природан, планински амбијент</li>
+                    </ul>
+                </div>
+                </article>
+            </aside>
         </div>
      );
 }
--- vikendica-miki/pages/index.js
+++ vikendica-miki/pages/index.js
@@ -6,19 +6,19 @@ export default function Home() {
   return (
     <div className="home-wrapper">
       <main>
-        <div className="home-hero-wrapper">
-          <div className="home-hero-text-wrapper">
+        <div className="home-hero hero-wrapper">
+          <div className="hero-text-wrapper">
             <h1>ВИКЕНДИЦА<br/>МИКИ</h1>
             <p>Уживајте у лепотама<br/>Таре</p>
           </div>
         </div>
       </main>
       <aside>
-        <div className="mountain-content">
+        <article className="first-container">
           <h2>Планина Тара</h2>
           <p>Спада у један од пет Националних паркова Србије и станисте је многих ретких биљних и животињских врста. Прелепи видиковци са којих се пружају незаборавни погледи само су неки од многобројних атракција које ће свима, који на тој планини одлуче да одморе од свакодневне градске гужве, пружити несвакидашње искуство.</p>
-        </div>
-        <div className="map-business-wrapper">
+        </article>
+        <article className="second-container">
           <div className="map-container">
             <label htmlFor="map">ЛОКАЦИЈА</label>
             <iframe id="map" className="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2873.338952903434!2d19.4627349157837!3d43.93165834239713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4759b3ccf19eff1d%3A0xf7c7f749416c73a3!2z0JLQuNC60LXQvdC00LjRhtCwINCc0JjQmtCY!5e0!3m2!1sen!2srs!4v1668777006334!5m2!1sen!2srs" allowFullScreen="" loading="lazy"></iframe>
@@ -32,7 +32,7 @@ export default function Home() {
               <li>У близини реке Дрине, језера Перућац и Заовине</li>
             </ul>
           </div>
-        </div> 
+        </article> 
         <h3>Савршено место за одмор!</h3> 
       </aside>
     </div>
